46ea24a758909b69eb7a2e36cf5545bcbf620cea
javeria2108 <javeriazh@gmail.com>
2024-09-27 20:16:37 +0500

    syncing

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='w-full border-b'>
+        <div className='wrapper flex items-center justify-between'>
+            <Link href="/" className='w-36'>
+            <Image src="/assets/images/logo.png" alt='logo' width={80} height={38}></Image>
+            </Link>
+            <div className='flex w-32 justify-end gap-3'>
+
+
+            </div>
+        </div>
+    </header>
   )
 }
 
